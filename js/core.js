16ee43ea6230efddd2eb8a820ba968286b639a44
dvdfans <70561512+dvdfans@users.noreply.github.com>
2026-08-23 19:20:20 +0800

    sync: 部署 2026-08-23 现役版本 (对齐 7116b6b0 沙箱, 含解析完整性校验)

--- js/core.js
+++ js/core.js
@@ -12,7 +12,7 @@ var ADMIN_LIST = [
 var STATS_API_URL = '';
 try {
   if ((location.hostname || '').indexOf('7116b6b0') >= 0) {
-    STATS_API_URL = 'https://shipments-asian-dos-campbell.trycloudflare.com/track';
+    STATS_API_URL = 'https://qualities-berkeley-incurred-story.trycloudflare.com/track';
   }
 } catch(e) {}
 
