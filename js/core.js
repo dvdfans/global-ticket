b8e68c64988ddbbf596b3e466d22f365fd59402d
dvdfans <70561512+dvdfans@users.noreply.github.com>
2026-09-21 11:39:38 +0800

    sync: 部署 2026-09-21 现役版本 (对齐 7116b6b0 沙箱, 含解析完整性校验)

--- js/core.js
+++ js/core.js
@@ -13,7 +13,7 @@ var STATS_API_URL = '';
 try {
   var _h = location.hostname || '';
   if (_h.indexOf('7116b6b0') >= 0 || _h.indexOf('305824db') >= 0 || _h.indexOf('7e62f7e0') >= 0 || /(app\.workbuddy\.host|app\.codebuddy\.work|agentos-app\.net|workbuddy\.link|github\.io)$/.test(_h)) {
-    STATS_API_URL = 'https://automobile-farms-operators-lexmark.trycloudflare.com';
+    STATS_API_URL = 'https://copied-belle-levels-dial.trycloudflare.com';
   }
 } catch(e) {}
 
